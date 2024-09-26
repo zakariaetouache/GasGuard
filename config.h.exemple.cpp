194191d7499325f2ab70fde85c2a793ea17dd38f
Zakariae Touache <zakariaetouache@gmail.com>
2024-09-26 19:10:11 +0000

    add config.h.exemple

---
+++ config.h.exemple.cpp
@@ -0,0 +1,16 @@
+// config.h.example
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Informations Wi-Fi (à remplacer par vos propres informations)
+const char* ssid = "YourSSID";
+const char* password = "YourPassword";
+
+// Token du bot Telegram (à remplacer par votre propre token du bot)
+#define BOTtoken "YourTelegramBotToken" 
+
+// ID du chat Telegram (à remplacer par votre propre ID de chat)
+#define CHAT_ID "YourTelegramChatID"  
+
+#endif
+
